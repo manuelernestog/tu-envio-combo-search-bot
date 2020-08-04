81e7b9068f5beef4c08fe6fd0e19f179f4e92579
Manuel <merodriguez9112@gmail.com>
2020-08-04 16:13:26 -0400

    Fixing orthography problems

--- modules/bot-module.js
+++ modules/bot-module.js
@@ -19,17 +19,15 @@ module.exports = {
     },
     help_message: function (msg) {
         const help_message = `<b>\u{1F449} Que es TuEnvioComboSearchBot?</b>
-Es un robot  que loacaliza nuevos productos en las tiendas virtuales TuEnvio y los publica en canales de Telegram segun su provincia.
+Es un robot  que localiza nuevos productos en las tiendas virtuales TuEnvio y los publica en canales de Telegram según su provincia.
 <b>\u{1F449} Porque se creo este robot ?</b>
-La tienda virtual TuEnvio no cuentan con un sisitema para notificar la publicación de nuevos productos. El robot se encarga de este trabajo, notificando a todos los usuarios subscritos a los diferentes canales de difusión.
+La tienda virtual TuEnvio no cuentan con un sistema para notificar la publicación de nuevos productos. El robot se encarga de este trabajo, notificando a todos los usuarios subscritos a los diferentes canales de difusión.
 <b>\u{1F449} Como funciona ?</b>
-El robot busca sistematicamente en las tiendas virtuales nuevos productos. una vez encontrados los publica en el canal que le corresponda, notifificando asi a todos los usuarios subscritos.
-<b>\u{1F449} Es mejor el robot que buscar personalmente?</b>
-El robot trabaja los 7 dias de la semana de 8am a 6pm sin interrupciones, revisando todas las tiendas del país en cuestion de segundos. Saque usted sus propias concluciones :)
+El robot trabaja los 7 días de la semana de 8am a 6pm sin interrupciones, revisando todas las tiendas del país buscando nuevos productos. Una vez encontrados los publica en el canal que le corresponda, notificando a todos los usuarios subscritos.
 <b>\u{1F449} Porque un canal y no una aplicación?</b>
-La publicación en un canal facilita que la busqueda en las tienda se realize una sola vez efectuándose desde el servidor donde está alojado el robot, las aplicaciones con este fin impactan negativamente en la estabilidad de la tienda ya las búsquedas se realizan desde cada terminal (Telefono), sobrecargando el sitio y haciéndolo inaccesible en ocasiones.
+La publicación en un canal facilita que la búsqueda en las tiendas se realice una sola vez efectuándose desde el servidor donde está alojado el robot, las aplicaciones con este fin impactan negativamente en la estabilidad de la tienda ya las búsquedas se realizan desde cada terminal, sobrecargando el sitio y haciéndolo inaccesible en ocasiones.
 <b>\u{1F449} Quien desarrolló el robot ?</b>   
-El robot es desarrollado por el Proyecto Zuntek, conformado por ingenieros informáticos cubanos con el objetivo de contribuir con la informatización de la sociedad mediante el desarrollo de soluciones informáticas para la poblacion cubana.
+El robot es desarrollado por el Proyecto Zuntek, conformado por ingenieros informáticos cubanos con el objetivo de contribuir con la informatización de la sociedad mediante el desarrollo de soluciones informáticas para la población cubana.
 `;
         bot.sendMessage(msg.chat.id, help_message, {parse_mode: 'HTML'});
     }
