8b278636c30633ae8f39cba0a3bc01af33837666
Manuel <merodriguez9112@gmail.com>
2020-08-27 12:02:43 -0400

    updating schedule time

--- modules/schedule-module.js
+++ modules/schedule-module.js
@@ -1,6 +1,5 @@
 const cron1 = require("node-cron");
 const cron2 = require("node-cron");
-const cron3 = require("node-cron");
 const bot_module = require('./bot-module');
 const file_module = require('./file-module');
 const product_list_operations = require('./products-list-operations');
@@ -13,17 +12,20 @@ module.exports.init_schedule = function () {
         file_module.remove_public_folder();
         file_module.crete_public_folder();
     });
-    cron1.schedule('*/2 8-10 * * *', () => {
-        review_habana_stores();
+    cron2.schedule('0 8 */7 * *', () => {
+        bot_module.send_zuntek_info_to_all();
     });
-    cron1.schedule('*/4 11 * * *', () => {
+    cron1.schedule('* 7-16 * * *', () => {
         review_habana_stores();
-    });
-    cron2.schedule('*/6 8-11 * * *', () => {
         review_cuba_stores();
     });
-    cron3.schedule('0 12 */5 * *', () => {
-        bot_module.send_zuntek_info_to_all();
+    cron1.schedule('*/5 17-23 * * *', () => {
+        review_habana_stores();
+        review_cuba_stores();
+    });
+    cron1.schedule('*/30 0-6 * * *', () => {
+        review_habana_stores();
+        review_cuba_stores();
     });
 };
 
